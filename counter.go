b6a41a04cccdfc6c776d6316728aa807ff2e67a9
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-05-13 16:32:17 +0200

    make metrics concurrent and added defer method

--- counter.go
+++ counter.go
@@ -2,14 +2,16 @@ package god
 
 import (
 	"fmt"
+	"sync"
 )
 
 // Counter is a simple operations counter.
 type Counter struct {
 	total int
+	mutex sync.Mutex
 }
 
-// NewCounter will create and return a Counter.
+// NewCounter will create and return a counter.
 func NewCounter(name string) *Counter {
 	c := &Counter{}
 	add(name, c)
@@ -18,13 +20,22 @@ func NewCounter(name string) *Counter {
 
 // Add will increment the counter.
 func (c *Counter) Add(n int) {
+	c.mutex.Lock()
 	c.total += n
+	c.mutex.Unlock()
 }
 
 func (c *Counter) string() string {
-	return fmt.Sprintf("%.2f c/s", float64(c.total))
+	// get value
+	c.mutex.Lock()
+	total := c.total
+	c.mutex.Unlock()
+
+	return fmt.Sprintf("%.2f c/s", float64(total))
 }
 
 func (c *Counter) reset() {
+	c.mutex.Lock()
 	c.total = 0
+	c.mutex.Unlock()
 }
--- timer.go
+++ timer.go
@@ -2,27 +2,42 @@ package god
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-// Timer is a simple operations Timer.
+// Timer is a simple operations timer.
 type Timer struct {
-	list []time.Duration
+	list  []time.Duration
+	mutex sync.Mutex
 }
 
-// NewTimer will create and return a Timer.
+// NewTimer will create and return a timer.
 func NewTimer(name string) *Timer {
 	t := &Timer{}
 	add(name, t)
 	return t
 }
 
-// Add will add the duration to the Timer.
+// Add will add the duration to the timer.
 func (t *Timer) Add(d time.Duration) {
+	t.mutex.Lock()
 	t.list = append(t.list, d)
+	t.mutex.Unlock()
+}
+
+// Measure can be used to track a function using defer.
+func (t *Timer) Measure() func() {
+	now := time.Now()
+	return func() {
+		t.Add(time.Since(now))
+	}
 }
 
 func (t *Timer) string() string {
+	// lock mutex
+	t.mutex.Lock()
+
 	// prepare mean
 	var min time.Duration
 	var mean time.Duration
@@ -57,9 +72,14 @@ func (t *Timer) string() string {
 		mean = sum / time.Duration(len(t.list))
 	}
 
+	// unlock mutex
+	t.mutex.Unlock()
+
 	return fmt.Sprintf("%s - %s - %s", min.String(), mean.String(), max.String())
 }
 
 func (t *Timer) reset() {
+	t.mutex.Lock()
 	t.list = nil
+	t.mutex.Unlock()
 }
