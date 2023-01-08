84b758bff5b82c114b6f4601a77f035339c33447
Joël Gähwiler <joel.gaehwiler@gmail.com>
2023-01-08 12:04:32 +0100

    test with separate binary

---
+++ test/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/256dpi/god"
+)
+
+func main() {
+	// init
+	god.Init(god.Options{})
+
+	// block events
+	go func() {
+		for {
+			<-time.After(time.Millisecond)
+		}
+	}()
+
+	// mutex events
+	var m sync.Mutex
+	for i := 0; i < 2; i++ {
+		go func() {
+			for {
+				m.Lock()
+				time.Sleep(time.Millisecond)
+				m.Unlock()
+			}
+		}()
+	}
+
+	// trace
+	go func() {
+		for {
+			func() {
+				e := god.Trace("foo", "bar")
+				defer e.End()
+				time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+			}()
+		}
+	}()
+
+	// metrics
+	counter := god.NewCounter("counter", nil)
+	timer := god.NewTimer("timer")
+	god.TrackInt("int", func() int64 { return 2 })
+	god.TrackFloat("float", func() float64 { return 4.2 })
+	go func() {
+		for {
+			d := time.Duration(rand.Intn(500)) * time.Millisecond
+			time.Sleep(d)
+			counter.Add(1)
+			timer.Add(d)
+		}
+	}()
+
+	select {}
+}
